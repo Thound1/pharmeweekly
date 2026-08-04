da1d651c3093fdab7d1f01d80f885ec009e5a167
Thound1 <inhakb2@gmail.com>
2026-08-04 13:43:00 +0900

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 window.APP_CONFIG = {
   APP_NAME: "파메어스 플랫폼기획팀 주간보고",
   TEAM_NAME: "플랫폼기획팀",
-  DEMO_MODE: true,
+  DEMO_MODE: false,
 
   GOOGLE_CLIENT_ID: "473271688404-k4u2952s2mbngbg9rp05f1psnss4sh6n.apps.googleusercontent.com",
   SPREADSHEET_ID: "1Tm94F-27tOoZV2mVsT05hgIJP5gw6jq4NeIRbWRI2Cs",
